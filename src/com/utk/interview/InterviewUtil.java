0e7586c767539c2ffac3261e3e37ed7f791e30cb
Utkarsh <ignition3951@gmail.com>
2025-11-05 12:35:46 +0530

    Implement callable interface

--- src/com/utk/interview/InterviewUtil.java
+++ src/com/utk/interview/InterviewUtil.java
@@ -1,9 +1,25 @@
 package com.utk.interview;
 
+import java.util.concurrent.*;
+
 public class InterviewUtil {
 
     public static void main(String[] args) {
-        MathOperation add = (a,b) -> System.out.println(a+b);
-        add.operate(5,3);
+        MathOperation add = (a, b) -> System.out.println(a + b);
+        add.operate(5, 3);
+
+        Callable<String> task = () -> {
+            Thread.sleep(1000);
+            return "task completed";
+        };
+        try(ExecutorService executor = Executors.newSingleThreadExecutor()) {
+
+        Future<String> future = executor.submit(task);
+
+            String result = future.get();
+            System.out.println("The result from the future is:" + result);
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
