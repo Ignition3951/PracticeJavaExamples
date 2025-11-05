598f8697fb54c30085e2cba87cecb6ea8c08f890
Utkarsh <ignition3951@gmail.com>
2025-11-05 12:13:44 +0530

    Implement functional interface

---
+++ src/com/utk/interview/InterviewUtil.java
@@ -0,0 +1,9 @@
+package com.utk.interview;
+
+public class InterviewUtil {
+
+    public static void main(String[] args) {
+        MathOperation add = (a,b) -> System.out.println(a+b);
+        add.operate(5,3);
+    }
+}
---
+++ src/com/utk/interview/MathOperation.java
@@ -0,0 +1,7 @@
+package com.utk.interview;
+
+@FunctionalInterface
+public interface MathOperation {
+
+    void operate(int a, int b);
+}
