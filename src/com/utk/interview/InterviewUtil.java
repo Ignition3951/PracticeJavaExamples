2447ada9f30a911229da8ef1ceb9ff564bd91a90
Utkarsh <ignition3951@gmail.com>
2025-11-06 11:46:09 +0530

    Implement map intermediate function

--- src/com/utk/interview/InterviewUtil.java
+++ src/com/utk/interview/InterviewUtil.java
@@ -1,7 +1,10 @@
 package com.utk.interview;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.function.*;
+import java.util.stream.Collectors;
 
 public class InterviewUtil {
 
@@ -13,9 +16,9 @@ public class InterviewUtil {
             Thread.sleep(1000);
             return "task completed";
         };
-        try(ExecutorService executor = Executors.newSingleThreadExecutor()) {
+        try (ExecutorService executor = Executors.newSingleThreadExecutor()) {
 
-        Future<String> future = executor.submit(task);
+            Future<String> future = executor.submit(task);
 
             String result = future.get();
             System.out.println("The result from the future is:" + result);
@@ -23,18 +26,18 @@ public class InterviewUtil {
             throw new RuntimeException(e);
         }
 
-        Predicate<String> isNotEmpty = s -> s!=null && !s.isEmpty();
+        Predicate<String> isNotEmpty = s -> s != null && !s.isEmpty();
 
         System.out.println(isNotEmpty.test("Hello"));
         System.out.println(isNotEmpty.test(""));
         System.out.println(isNotEmpty.test(null));
 
         //isPositive
-        IntPredicate isPositive = n -> n>0;
+        IntPredicate isPositive = n -> n > 0;
         System.out.println(isPositive.test(-5));
         System.out.println(isPositive.test(5));
         //isNotEmptyAndStartsWithA
-        Predicate<String> isNotEmptyAndStartsWithA = isNotEmpty.and(s-> s.startsWith("A"));
+        Predicate<String> isNotEmptyAndStartsWithA = isNotEmpty.and(s -> s.startsWith("A"));
         System.out.println(isNotEmptyAndStartsWithA.test("asdadasd"));
         System.out.println(isNotEmptyAndStartsWithA.test("Asdadasd"));
 
@@ -45,17 +48,23 @@ public class InterviewUtil {
         UnaryOperator<String> toUpperCase = String::toUpperCase;
         System.out.println(toUpperCase.apply("Hello"));
         //upperCaseLength
-        Function<String,Integer> toUpperCaseAndThenLength = toUpperCase.andThen(String::length);
+        Function<String, Integer> toUpperCaseAndThenLength = toUpperCase.andThen(String::length);
         System.out.println(toUpperCaseAndThenLength.apply("hello first"));
 
-        Consumer<String> printConsumer = s -> System.out.println("Consumed value is : "+s);
+        Consumer<String> printConsumer = s -> System.out.println("Consumed value is : " + s);
         printConsumer.accept("Hello");
 
-        Consumer<String> convertToUppercase = s -> System.out.println("The converted value is : "+s.toUpperCase());
+        Consumer<String> convertToUppercase = s -> System.out.println("The converted value is : " + s.toUpperCase());
         convertToUppercase.accept("dummy value");
 
         Consumer<String> convertAndPrint = printConsumer.andThen(convertToUppercase);
         convertAndPrint.accept("print and convert");
+
+        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+        List<String> upperCaseNames = names.stream()
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println(upperCaseNames);
 
     }
 }
